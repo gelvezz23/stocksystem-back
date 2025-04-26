c2085fc2a40ff7c39bd81b313cad7bc48f32499d
gelvezz23 <mao_23_@hotmail.com>
2025-04-26 15:25:06 -0500

    feat: add servicio tecnico

--- routes/clientes.js
+++ routes/clientes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get("/clientes", clientesController.getAllClientes);
 router.get("/clientes/:id", clientesController.getClienteById);
+router.get("/clientes/tecnico", clientesController.getTecnicos);
 router.post("/clientes", clientesController.createCliente);
 router.put("/clientes/:id", clientesController.updateCliente);
 router.delete("/clientes/:id", clientesController.deleteCliente);
